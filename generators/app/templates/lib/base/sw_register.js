07c1173762878757e0324a46251fc11ecb02bec3
huyaFed <303182519@qq.com>
2017-12-01 18:37:00 +0800

    fix:修改微信的刷新问题

--- generators/app/templates/lib/base/sw_register.js
+++ generators/app/templates/lib/base/sw_register.js
@@ -34,31 +34,42 @@ if('serviceWorker' in navigator && location.protocol === 'https:') {
                     scope: './'
                 }).then(function(registration) {
 					console.log('ServiceWorker registration successful with scope: ', registration.scope);
+					function isWeixn(){  
+					    var ua = navigator.userAgent.toLowerCase();  
+					    if(ua.match(/MicroMessenger/i)=="micromessenger") {  
+					        return true;  
+					    } else {  
+					        return false;  
+					    }  
+					}  
 
-				    navigator.serviceWorker.addEventListener('message', function(e) {
-				        // service-worker.js 如果更新成功会 postMessage 给页面，内容为 'sw.update'
-				        if (e.data === 'sw.update') {
-				            var dom = document.createElement('div');
-				            var themeColor = document.querySelector('meta[name=theme-color]');
-				            themeColor && (themeColor.content = '#000');
-				            dom.innerHTML = '<style>'+
-				                    '.app-refresh{background:#000;height:0;line-height:52px;overflow:hidden;position:fixed;top:0;left:0;right:0;z-index:10001;padding:0 18px;transition:all .3s ease;-webkit-transition:all .3s ease;-moz-transition:all .3s ease;-o-transition:all .3s ease;}'+
-				                    '.app-refresh-wrap{display:flex;color:#fff;font-size:15px;}'+
-				                    '.app-refresh-wrap label{flex:1;}'+
-				                    '.app-refresh-show{height:52px;}'+
-				                '</style>'+
-				                '<div class="app-refresh" id="app-refresh">'+
-				                    '<div class="app-refresh-wrap" onclick="location.reload()">'+
-				                        '<label>已更新最新版本</label>'+
-				                        '<span>点击刷新</span>'+
-				                    '</div>'+
-				                '</div>';
-				            document.body.appendChild(dom);
-				            setTimeout(function(){
-				            	document.getElementById('app-refresh').className += ' app-refresh-show'
-				            }, 16);
-				        }
-				    });
+					if (!isWeixn()) {
+						navigator.serviceWorker.addEventListener('message', function(e) {
+						    // service-worker.js 如果更新成功会 postMessage 给页面，内容为 'sw.update'
+						    if (e.data === 'sw.update') {
+						        var dom = document.createElement('div');
+						        var themeColor = document.querySelector('meta[name=theme-color]');
+						        themeColor && (themeColor.content = '#000');
+						        dom.innerHTML = '<style>'+
+						                '.app-refresh{background:#000;height:0;line-height:52px;overflow:hidden;position:fixed;top:0;left:0;right:0;z-index:10001;padding:0 18px;transition:all .3s ease;-webkit-transition:all .3s ease;-moz-transition:all .3s ease;-o-transition:all .3s ease;}'+
+						                '.app-refresh-wrap{display:flex;color:#fff;font-size:15px;}'+
+						                '.app-refresh-wrap label{flex:1;}'+
+						                '.app-refresh-show{height:52px;}'+
+						            '</style>'+
+						            '<div class="app-refresh" id="app-refresh">'+
+						                '<div class="app-refresh-wrap" onclick="location.reload()">'+
+						                    '<label>已更新最新版本</label>'+
+						                    '<span>点击刷新</span>'+
+						                '</div>'+
+						            '</div>';
+						        document.body.appendChild(dom);
+						        setTimeout(function(){
+						        	document.getElementById('app-refresh').className += ' app-refresh-show'
+						        }, 16);
+						    }
+						});
+					}
+				    
 				}).catch(function(err) {
 	                console.log('ServiceWorker registration err: ', err);
 	            });
